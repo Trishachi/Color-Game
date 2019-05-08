05a01381987349d7f8d1e17070c08f0139b99806
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2019-05-08 15:10:49 -0600

    Completed Colour generation and selection

--- colourGame.js
+++ colourGame.js
@@ -7,8 +7,9 @@ let colors = ["rgb(255, 0, 0)",
               "rgb(255, 0, 255)"
             ]
 let squares = document.querySelectorAll(".square");
-let goalColor = colors[3];
+let goalColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
+let messageDisplay = document.querySelector("#message");
 
 
 // Set goal colour
@@ -25,9 +26,26 @@ for(var i = 0; i < squares.length; i++){
     var clickedColor = this.style.backgroundColor;
     //compare color to goalColor
     if(clickedColor === goalColor){
-      alert("Correct!");
+      messageDisplay.textContent = "Correct!";
+      changeColors(clickedColor);
       }  else {
-        alert("WRONG");
+        this.style.backgroundColor = "#232323";
+        messageDisplay.textContent = "Try Again";
       }
   });
+}
+
+//Function for Changing square colours
+function changeColors(color){
+  //loo through all squares
+  for(var i = 0; i < squares.length; i++){
+      //change color to match selected coloue
+    squares[i].style.backgroundColor = color;
+  }
+}
+
+// Function for generating/picking goal colour
+function pickColor(){
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
 }
