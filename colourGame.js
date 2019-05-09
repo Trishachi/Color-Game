b91381046552c2290381d80c6753f79c479ed9c6
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2019-05-09 14:59:13 -0600

    Reset Button Functionality Completed

--- colourGame.js
+++ colourGame.js
@@ -5,7 +5,23 @@ let goalColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
 let messageDisplay = document.querySelector("#message");
 let h1 = document.querySelector("h1");
+let resetButton = document.querySelector("#reset");
 
+
+//Reset Button Codes
+resetButton.addEventListener("click", function(){
+  //generate all new colours
+  colors = generateRandomColors(6);
+  //pick random color from array
+  goalColor = pickColor();
+  //change colour display to matched goal color
+  colorDisplay.textContent = goalColor;
+  // change squre colors
+  for(var i = 0; i < squares.length; i++){
+    squares[i].style.backgroundColor = colors[i];
+  }
+  h1.style.backgroundColor = "#232323";
+});
 
 // Set goal colour
 colorDisplay.textContent = goalColor;
@@ -22,6 +38,7 @@ for(var i = 0; i < squares.length; i++){
     // console.log(clickedColor, goalColor);
     if(clickedColor === goalColor){
       messageDisplay.textContent = "Correct!";
+      resetButton.textContent = "Play Again?";
       h1.style.backgroundColor = clickedColor;
       changeColors(clickedColor);
       }  else {
@@ -46,7 +63,7 @@ function pickColor(){
   return colors[random];
 }
 
-//Function for generating random colours in square boxes
+//Functions for generating random colours in square boxes
 function generateRandomColors(num){
   //make an array
   var arr = [];
